7691fd20f7a61f6e1863347ec87ddb4830f4fe91
er0k <er0k@er0k.net>
2025-04-11 20:31:42 -0400

    🌝

---
+++ README.md
@@ -0,0 +1,91 @@
+# slack.sh
+
+Set your Slack status, presence and do-not-disturb from the shell.
+
+If you are the current on-call, get a random status:
+
+:feelsgood: :finnadie: :goberserk: :godmode: :hurtrealbad: :rage1: :rage2: :rage3: :rage4: :suspect: _on-call_
+
+- `here`: here and not snoozed (dnd off). No status, unless you are on-call
+- `away`: away and snoozed (dnd on) for 24 hours. If on-call, no snoozing
+- `zzz`: here and snoozed (unless you are on-call)
+
+Some other (maybe) useful statuses:
+
+- `lunch`: away and snoozed for 1 hour. Random status of :pizza: :hamburger: :taco: :sandwich: :burrito: _Eating lunch_
+- `brb`: away and snooze for 5 minutes. :walking: _afk brb_
+- `dog`: away and snooze for 20 minutes. :walking-the-dog: _afk brb_
+- `meet`: here and snoozed. :calendar: _In a meeting_
+- `call`: here and snoozed. :telephone_receiver: _On a call_
+- `pom`: here and snooze for 25 minutes. Useful for pomodoro.
+
+other commands:
+
+- `st`: get the current status, skipping cache
+- `stc`: get the cached status (cache lives for 5 minutes)
+- `moon`: a trip to the moon :new_moon_with_face:
+- `zoom`: run this in a cronjob to check if you are using zoom and set `meet` accordingly
+
+## Install
+
+1. Make sure `curl`, `jq`, `jo`, `pgrep` are available in your `$PATH`
+
+2. Create a [new Slack App](https://api.slack.com/apps?new_app=1) from scratch
+
+3. Grant your app the following OAuth scopes:
+
+    - users.profile:read
+    - users.profile:write
+    - users:read
+    - users:write
+    - dnd:read
+    - dnd:write
+
+4. Install the app to your Workspace to get the OAuth token (probably need admin approval for this step, so ask nicely)
+
+5. Get the necessary environment variables
+
+    + `SLACK_USER_ID` from `https://<your-org>.slack.com/account/profile`
+    + `SLACK_TOKEN` from step 4 above
+    + `PD_USER_ID` from `https://<your-org>.pagerduty.com/users-new`
+    + `PD_TOKEN` from ... I forget
+    + `PD_SHEDULE_ID` from ... I forget
+
+6. Set the env vars:
+
+    ```
+    export SLACK_USER_ID="your-slack-id"
+    export SLACK_TOKEN="your-slack-token"
+    export PD_USER_ID="your-pagerduty-id"
+    export PD_TOKEN="your-pagerduty-token"
+    export PD_SHEDULE_ID="your-pagerduty-schedule-id"
+    ```
+
+## Usage
+
+Run the script for usage instructions:
+
+```
+$ ./slack.sh
+```
+
+To run in a cronjob, you can use a tmux session with your env vars in it:
+
+```
+* 10-18 * * 1-5 tmux send -t cron "/path/to/slack.sh zoom" ENTER > /dev/null 2>&1
+```
+
+To put your slack status in your tmux bar, do something like:
+
+```
+set-option -g status-right "\
+#[fg=colour15, bg=colour240]\
+#[fg=colour13, bg=colour237] #(/path/to/slack.sh stc) \
+#[fg=colour15, bg=colour240]"
+```
+
+You may want to set an alias if are lazy and hate typing, like me:
+
+```
+alias sl='/path/to/slack.sh'
+```
---
+++ slack.sh
@@ -0,0 +1,268 @@
+#!/bin/bash
+# set slack status/presence/dnd
+
+# set these in your env
+[ -n "$SLACK_USER_ID" ] || { echo "need a SLACK_USER_ID to continue"; exit 2; }
+[ -n "$SLACK_TOKEN" ] || { echo "need a SLACK_TOKEN to continue"; exit 3; }
+
+# set these if you want to check pagerduty
+: "${PD_USER_ID:=nope}"
+: "${PD_TOKEN:=nope}"
+: "${PD_SHEDULE_ID:=nope}"
+
+# cached slack status so we don't crush the API
+STATUS_FILE=/tmp/slack_status
+
+# check for necessary tools
+tools="curl jq jo pgrep"
+for tool in $tools; do
+  command -v "$tool" > /dev/null || { echo "missing $tool, cannot continue"; exit 4; }
+done
+
+rand_food() {
+  foods=("pizza" "hamburger" "taco" "sandwich" "burrito" "ramen")
+  echo "${foods[$RANDOM % ${#foods[@]}]}"
+}
+
+rand_doom() {
+  doom=("feelsgood" "finnadie" "goberserk" "godmode" "hurtrealbad" "rage1" "rage2" "rage3" "rage4" "suspect")
+  echo "${doom[$RANDOM % ${#doom[@]}]}"
+}
+
+# https://api.slack.com/methods/users.profile.set
+set_status() {
+  status="${1:-}"
+  emoji="${2:-}"
+  echo -n "setting status ${status} ${emoji}.."
+  [ -z "$emoji" ] || emoji="\:$emoji:"
+  body_profile=$(jo profile="$(jo status_text="$status" status_emoji="$emoji" status_expiration=0)")
+  curl -s -X POST \
+    -H "Content-type: application/json; charset=utf-8" \
+    -H "Authorization: Bearer ${SLACK_TOKEN}" \
+    -H "X-Slack-User: ${SLACK_USER_ID}" \
+    -d "${body_profile}" \
+    https://slack.com/api/users.profile.set | jq .ok
+  rm -f "${STATUS_FILE}"
+}
+
+# https://api.slack.com/methods/users.setPresence
+set_presence() {
+  pres="${1:-auto}" # auto or away
+  echo -n "setting presence ${pres}.."
+  body_presence=$(jo presence="$pres")
+  curl -s -X POST \
+    -H "Content-type: application/json; charset=utf-8" \
+    -H "Authorization: Bearer ${SLACK_TOKEN}" \
+    -H "X-Slack-User: ${SLACK_USER_ID}" \
+    -d "${body_presence}" \
+    https://slack.com/api/users.setPresence | jq .ok
+}
+
+get_presence() {
+  curl -s \
+    -H "Content-type: application/json; charset=utf-8" \
+    -H "Authorization: Bearer ${SLACK_TOKEN}" \
+    -H "X-Slack-User: ${SLACK_USER_ID}" \
+    https://slack.com/api/users.getPresence \
+    | jq -r .presence
+}
+
+get_status() {
+  curl -s \
+    -H "Content-type: application/json; charset=utf-8" \
+    -H "Authorization: Bearer ${SLACK_TOKEN}" \
+    -H "X-Slack-User: ${SLACK_USER_ID}" \
+    https://slack.com/api/users.profile.get \
+    | jq -r .profile.status_text # .profile.status_emoji
+}
+
+# snooze aka do-not-disturb
+is_snoozing() {
+  snoozing=$(curl -s -X POST \
+  -H "Content-type: application/json; charset=utf-8" \
+  -H "Authorization: Bearer ${SLACK_TOKEN}" \
+  -H "X-Slack-User: ${SLACK_USER_ID}" \
+  https://slack.com/api/dnd.info | jq .snooze_enabled)
+  [[ "${snoozing}" == "true" ]]
+}
+
+snooze() {
+  echo -n "snoozing for.."
+  mins="${1:-1440}" # snooze for a day
+  body_snooze=$(jo num_minutes="${mins}")
+  curl -s -X POST \
+    -H "Content-type: application/json; charset=utf-8" \
+    -H "Authorization: Bearer ${SLACK_TOKEN}" \
+    -H "X-Slack-User: ${SLACK_USER_ID}" \
+    -d "${body_snooze}" \
+    "https://slack.com/api/dnd.setSnooze" | jq .snooze_remaining
+}
+
+wake_up() {
+  echo -n "waking up.."
+  curl -s -X POST \
+    -H "Content-type: application/json; charset=utf-8" \
+    -H "Authorization: Bearer ${SLACK_TOKEN}" \
+    -H "X-Slack-User: ${SLACK_USER_ID}" \
+    "https://slack.com/api/dnd.endSnooze" | jq .ok
+}
+
+is_linux() {
+  [[ $(uname -s) == "Linux" ]]
+}
+
+is_mac() {
+  [[ $(uname -s) == "Darwin" ]]
+}
+
+using_zoom() {
+  if is_linux; then
+    [[ $(pgrep -i zoom) ]] && return
+  fi
+
+  if is_mac; then
+    [[ $(pgrep -i cpthost) ]] && return
+  fi
+
+  false
+}
+
+is_oncall() {
+  [ "$PD_USER_ID" = "nope" ] && return 1
+  [ "$PD_TOKEN" = "nope" ] && return 1
+  [ "$PD_SHEDULE_ID" = "nope" ] && return 1
+  oncall_id=$(curl -s \
+    -H "Authorization: Token token=$PD_TOKEN" \
+    "https://api.pagerduty.com/oncalls?schedule_ids[]=$PD_SHEDULE_ID" \
+    | jq -r .oncalls[1].user.id \
+  )
+  if [[ "$oncall_id" == "$PD_USER_ID" ]]; then
+    echo "you're on call. hooray"
+    return
+  else
+    false
+  fi
+}
+
+idle_time() {
+  if is_linux; then
+    echo $(($(xssstate -i) / 1000))
+  fi
+
+  if is_mac; then
+    /usr/sbin/ioreg -c IOHIDSystem | awk '/HIDIdleTime/ {print int($NF/1000000000)}'
+  fi
+}
+
+get_live_status() {
+  snooze=$(is_snoozing && echo "🔕" || echo "")
+  presence=$([ $(get_presence) == "away" ] && echo "🔴" || echo "🟢")
+  echo "${presence} ${snooze} $(get_status)" > "${STATUS_FILE}"
+}
+
+get_cached_status() {
+  if [ ! -f "${STATUS_FILE}" ]; then
+    get_live_status
+  fi
+
+  # 5 minutes
+  if [ $(stat --format=%Y "${STATUS_FILE}") -le $(( `date +%s` - (5*60) )) ]; then
+    get_live_status
+  fi
+}
+
+case "$1" in
+  zoom)
+    status="$(get_status)"
+    if using_zoom; then
+      echo "zoomin"
+      if [[ "${status}" == "In a meeting" ]]; then
+        echo "already in a meeting"
+      else
+        $0 meet
+      fi
+    else
+      if [[ "${status}" == "In a meeting" ]]; then
+        $0 here
+      fi
+    fi
+    ;;
+  here)
+    wake_up
+    set_presence
+    if is_oncall; then
+      set_status "on-call" "$(rand_doom)"
+    else
+      set_status
+    fi
+    ;;
+  away)
+    set_presence away
+    if is_oncall; then
+      set_status "on-call" "$(rand_doom)"
+      wake_up
+    else
+      set_status
+      snooze
+    fi
+    ;;
+  lunch)
+    set_presence away
+    set_status "Eating lunch" "$(rand_food)"
+    snooze 60
+    ;;
+  brb)
+    set_presence away
+    set_status "afk brb" "walking"
+    snooze 5
+    ;;
+  dog)
+    set_presence away
+    set_status "afk brb" "walking-the-dog"
+    snooze 20
+    ;;
+  meet)
+    set_presence auto
+    set_status "In a meeting" "calendar"
+    snooze
+    ;;
+  call)
+    set_presence auto
+    set_status "On a call" "telephone_receiver"
+    snooze
+    ;;
+  zzz)
+    set_presence auto
+    if is_oncall; then
+      set_status "on-call" "$(rand_doom)"
+      echo "sorry, no snoozing. You are on-call."
+      wake_up
+    else
+      set_status
+      snooze
+    fi
+    ;;
+  st)
+    get_live_status
+    cat "${STATUS_FILE}"
+    ;;
+  stc)
+    get_cached_status
+    cat "${STATUS_FILE}"
+    ;;
+  pom)
+    snooze 25
+    ;;
+  moon)
+    emojis="new_moon waxing_crescent_moon first_quarter_moon moon full_moon waning_gibbous_moon last_quarter_moon waning_crescent_moon new_moon_with_face"
+    for emoji in $emojis; do
+      set_status "..." "${emoji}"
+      sleep 3
+    done
+    set_status
+    ;;
+  *)
+    echo "Usage: $0 [here|away|zzz|lunch|brb|dog|meet|call|pom|st|stc|zoom|moon]"
+    exit 1
+    ;;
+esac
